b11c401de318d48e1c42962c41605001734ebd6e
ZageGit <70768074+ZageGit@users.noreply.github.com>
2022-04-26 23:05:32 +0200

    adjust helper function

--- helper/transform.js
+++ helper/transform.js
@@ -1,15 +1,29 @@
 import dbConnect from "../util/mongo";
 import Product from "../models/Product";
 
-export default async function productHandler() {
+// export default async function productHandler() {
+//     await dbConnect();
+//     const result = await Product.find().lean();
+
+//     const newResult = result.map((p) => {
+//         const k = JSON.stringify(p);
+//         const l = JSON.parse(k)
+//         return l;
+//     })
+//     return newResult;
+// }
+
+export default async function handler(req, res) {
     await dbConnect();
-    const result = await Product.find().lean();
+    try {  const result = await Product.find().lean();
 
-    const newResult = result.map((p) => {
-        const k = JSON.stringify(p);
-        const l = JSON.parse(k)
-        return l;
-    })
-    return newResult;
-}
-
+        const newResult = result.map((p) => {
+            const k = JSON.stringify(p);
+            const l = JSON.parse(k)
+            return l;
+        })     
+      res.status(200).json({ newResult })
+    } catch (err) {
+      res.status(500).json({ error: 'failed to load data' })
+    }
+  }
