31997ccb61e8b4e436e63c2fc777485fd599d0a1
ZageGit <70768074+ZageGit@users.noreply.github.com>
2022-04-26 23:10:14 +0200

    add dbconnect

--- helper/transform.js
+++ helper/transform.js
@@ -1,4 +1,5 @@
 import Product from "../models/Product";
+import dbConnect from "../util/mongo";
 
 // export default async function productHandler() {
 //     await dbConnect();
@@ -13,7 +14,7 @@ import Product from "../models/Product";
 // }
 
 export default async function productHandler(req, res) {
-  
+  await dbConnect();
     try {  const result = await Product.find().lean();
 
         const newResult = result.map((p) => {
