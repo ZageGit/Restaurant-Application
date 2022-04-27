de28a054f2d133e29037a3c55e4283dcfe6c55a7
ZageGit <70768074+ZageGit@users.noreply.github.com>
2022-04-27 15:44:14 +0200

    adjust status condition

--- pages/api/orders/[id].js
+++ pages/api/orders/[id].js
@@ -7,7 +7,7 @@ const handler = async (req, res)=>{
     await dbConnect();
 
 
-    if(method === "GET"){
+    if(method === "GET" ){
         try {
             const order = await Order.findById(id);
             res.status(200).json(order);
@@ -15,7 +15,7 @@ const handler = async (req, res)=>{
             res.status(500).json(err);
           }
     }
-    if(method === "PUT"){
+    if(method === "PUT "&& req.body.status < 3){
         try {
             const order = await Order.findByIdAndUpdate(id, req.body, {
               new: true,
